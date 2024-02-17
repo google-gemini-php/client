d40550c2757466e95094ebdeb037c64a8e83f92c
Fatih Aydın <fatihaydin@temizlikyolda.com>
2024-02-17 10:34:38 +0300

    feat: Add `HasModel` trait

---
+++ src/Concerns/HasModel.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gemini\Concerns;
+
+use BackedEnum;
+use Gemini\Enums\ModelType;
+
+trait HasModel
+{
+    public function parseModel(ModelType|string $model): string
+    {
+        return match (true) {
+            $model instanceof BackedEnum => $model->value,
+            str_starts_with($model, 'models') => $model,
+            default => "models/$model"
+        };
+    }
+}
