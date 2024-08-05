91acfd5c5ccade465f4cb079386b999762eef670
Ferhat Nalbant <nalbantferhat@gmail.com>
2024-08-05 10:29:25 +0300

    feat: New Gemini releases
    gemini-1.0-pro, gemini-1.0-pro-latest, gemini-1.5-pro, gemini-1.5-flash
    
    #25

--- src/Enums/ModelType.php
+++ src/Enums/ModelType.php
@@ -7,6 +7,10 @@ namespace Gemini\Enums;
 enum ModelType: string
 {
     case GEMINI_PRO = 'models/gemini-pro';
+    case GEMINI_PRO_1_0 = 'models/gemini-1.0-pro';
+    case GEMINI_PRO_1_0_LATEST = 'models/gemini-1.0-pro-latest';
+    case GEMINI_PRO_1_5 = 'models/gemini-1.5-pro';
+    case GEMINI_PRO_1_5_FLASH = 'models/gemini-1.5-flash';
     case GEMINI_PRO_VISION = 'models/gemini-pro-vision';
     case EMBEDDING = 'models/embedding-001';
 }
