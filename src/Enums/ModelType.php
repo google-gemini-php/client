1feca91b26991e2f6909ee612f4bb274c7797004
Ali Raza <80054032+AliRaza-commits@users.noreply.github.com>
2024-08-20 16:26:53 +0500

    Update ModelType.php

--- src/Enums/ModelType.php
+++ src/Enums/ModelType.php
@@ -7,6 +7,6 @@ namespace Gemini\Enums;
 enum ModelType: string
 {
     case GEMINI_PRO = 'models/gemini-pro';
-    case GEMINI_PRO_VISION = 'models/gemini-pro-vision';
+    case GEMINI_PRO_VISION = 'models/gemini-1.5-flash';
     case EMBEDDING = 'models/embedding-001';
 }
