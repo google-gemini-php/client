eddf6bf403a30b87b2c3e312d4aca7bec5233129
Fatih Aydın <fatihaydin@temizlikyolda.com>
2024-09-28 15:39:44 +0300

    feat: Add `generateGeminiModel` method to `ModelType` enum

--- src/Enums/ModelType.php
+++ src/Enums/ModelType.php
@@ -11,4 +11,24 @@ enum ModelType: string
     case GEMINI_FLASH = 'models/gemini-1.5-flash';
     case EMBEDDING = 'models/embedding-001';
     case TEXT_EMBEDDING = 'models/text-embedding-004';
+
+    /**
+     * https://ai.google.dev/gemini-api/docs/models/gemini#model-variations
+     */
+    public static function generateGeminiModel(ModelVariation $variation, ?float $generation = null, ?string $version = null): string
+    {
+        $model = 'models/gemini';
+
+        if ($generation != null) {
+            $model .= '-'.number_format($generation, 1);
+        }
+
+        $model .= "-{$variation->value}";
+
+        if ($version) {
+            $model .= "-{$version}";
+        }
+
+        return $model;
+    }
 }
