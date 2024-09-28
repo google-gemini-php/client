d6a8485dd4f4403357844be4527e70efb418b061
Fatih Aydın <fatihaydin@temizlikyolda.com>
2024-09-28 15:38:49 +0300

    feat: Add `GEMINI_FLASH` and `TEXT_EMBEDDING` enums to `ModelType`

--- src/Enums/ModelType.php
+++ src/Enums/ModelType.php
@@ -8,5 +8,7 @@ enum ModelType: string
 {
     case GEMINI_PRO = 'models/gemini-pro';
     case GEMINI_PRO_VISION = 'models/gemini-pro-vision';
+    case GEMINI_FLASH = 'models/gemini-1.5-flash';
     case EMBEDDING = 'models/embedding-001';
+    case TEXT_EMBEDDING = 'models/text-embedding-004';
 }
