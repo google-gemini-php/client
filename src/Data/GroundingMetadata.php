28ac92b72ae5b96dcebbf6d28f4af1fd46fe0542
Maxime Tassy <maxime.tassy@infomaniak.com>
2025-05-12 14:05:11 +0200

    fix: groundingChunks, groundingSupports, webSearchQueries and retrievalMetadata are nullable

--- src/Data/GroundingMetadata.php
+++ src/Data/GroundingMetadata.php
@@ -14,54 +14,66 @@ use Gemini\Contracts\Arrayable;
 final class GroundingMetadata implements Arrayable
 {
     /**
-     * @param  array<GroundingChunk>  $groundingChunks  List of supporting references retrieved from specified grounding source.
-     * @param  array<GroundingSupport>  $groundingSupports  List of grounding support.
-     * @param  array<string>  $webSearchQueries  Web search queries for the following-up web search.
+     * @param  array<GroundingChunk>|null  $groundingChunks  List of supporting references retrieved from specified grounding source.
+     * @param  array<GroundingSupport>|null  $groundingSupports  List of grounding support.
+     * @param  array<string>|null  $webSearchQueries  Web search queries for the following-up web search.
      * @param  SearchEntryPoint|null  $searchEntryPoint  Optional. Google search entry for the following-up web searches.
-     * @param  RetrievalMetadata  $retrievalMetadata  Metadata related to retrieval in the grounding flow.
+     * @param  RetrievalMetadata|null  $retrievalMetadata  Metadata related to retrieval in the grounding flow.
      */
     public function __construct(
-        public readonly array $groundingChunks,
-        public readonly array $groundingSupports,
-        public readonly array $webSearchQueries,
+        public readonly ?array $groundingChunks,
+        public readonly ?array $groundingSupports,
+        public readonly ?array $webSearchQueries,
         public readonly ?SearchEntryPoint $searchEntryPoint,
-        public readonly RetrievalMetadata $retrievalMetadata,
+        public readonly ?RetrievalMetadata $retrievalMetadata,
     ) {}
 
     /**
-     * @param  array{ groundingChunks: array<array{ web?: array{ uri: string, title: string } }>, groundingSupports: array<array{ groundingChunkIndices: array<int>, confidenceScores: array<float>, segment: array{ partIndex: int, startIndex: int, endIndex: int, text: string } }>, webSearchQueries: array<string>, searchEntryPoint?: array{ renderedContent?: string|null, sdkBlob?: string|null }, retrievalMetadata: array{ googleSearchDynamicRetrievalScore?: float|null } }  $attributes
+     * @param  array{ groundingChunks: ?array<array{ web?: array{ uri: string, title: string } }>, groundingSupports: ?array<array{ groundingChunkIndices: array<int>, confidenceScores: array<float>, segment: array{ partIndex: int, startIndex: int, endIndex: int, text: string } }>, webSearchQueries: ?array<string>, searchEntryPoint?: array{ renderedContent?: string|null, sdkBlob?: string|null }, retrievalMetadata: ?array{ googleSearchDynamicRetrievalScore?: float|null } }  $attributes
      */
     public static function from(array $attributes): self
     {
         return new self(
-            groundingChunks: array_map(
-                static fn (array $groundingChunk): GroundingChunk => GroundingChunk::from($groundingChunk),
-                $attributes['groundingChunks'],
-            ),
-            groundingSupports: array_map(
-                static fn (array $groundingSupport): GroundingSupport => GroundingSupport::from($groundingSupport),
-                $attributes['groundingSupports'],
-            ),
-            webSearchQueries: $attributes['webSearchQueries'],
+            groundingChunks: match (true) {
+                isset($attributes['groundingChunks']) => array_map(
+                    static fn (array $groundingChunk): GroundingChunk => GroundingChunk::from($groundingChunk),
+                    $attributes['groundingChunks'],
+                ),
+                default => null
+            },
+            groundingSupports: match (true) {
+                isset($attributes['groundingSupports']) => array_map(
+                    static fn (array $groundingSupport): GroundingSupport => GroundingSupport::from($groundingSupport),
+                    $attributes['groundingSupports'],
+                ),
+                default => null
+            },
+            webSearchQueries: $attributes['webSearchQueries'] ?? null,
             searchEntryPoint: isset($attributes['searchEntryPoint']) ? SearchEntryPoint::from($attributes['searchEntryPoint']) : null,
-            retrievalMetadata: RetrievalMetadata::from($attributes['retrievalMetadata']),
+            retrievalMetadata: isset($attributes['retrievalMetadata']) ? RetrievalMetadata::from($attributes['retrievalMetadata']) : null,
         );
     }
 
     public function toArray(): array
     {
         return [
-            'groundingChunks' => array_map(
-                static fn (GroundingChunk $groundingChunk): array => $groundingChunk->toArray(),
-                $this->groundingChunks,
-            ),
-            'groundingSupports' => array_map(
-                static fn (GroundingSupport $groundingSupport): array => $groundingSupport->toArray(),
-                $this->groundingSupports,
-            ),
+            'groundingChunks' => match (true) {
+                isset($this->groundingChunks) => array_map(
+                    static fn (GroundingChunk $groundingChunk): array => $groundingChunk->toArray(),
+                    $this->groundingChunks,
+                ),
+                default => null
+            },
+            'groundingSupports' => match (true) {
+                isset($this->groundingSupports) => array_map(
+                    static fn (GroundingSupport $groundingSupport): array => $groundingSupport->toArray(),
+                    $this->groundingSupports,
+                ),
+                default => null
+            },
             'webSearchQueries' => $this->webSearchQueries,
             'searchEntryPoint' => $this->searchEntryPoint?->toArray(),
-            'retrievalMetadata' => $this->retrievalMetadata->toArray(),
+            'retrievalMetadata' => $this->retrievalMetadata?->toArray(),
         ];
     }
 }
