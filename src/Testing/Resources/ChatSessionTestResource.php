0c2d81f29ddd17711f306dcfb2529fb0f21c9861
Fatih Aydın <fatihaydin@temizlikyolda.com>
2025-11-22 18:03:11 +0300

    feat: Add streamSendMessage method to ChatSessionTestResource and corresponding test

--- src/Testing/Resources/ChatSessionTestResource.php
+++ src/Testing/Resources/ChatSessionTestResource.php
@@ -7,9 +7,11 @@ namespace Gemini\Testing\Resources;
 use Gemini\Contracts\Resources\ChatSessionContract;
 use Gemini\Data\Blob;
 use Gemini\Data\Content;
+use Gemini\Data\UploadedFile;
 use Gemini\Resources\ChatSession;
 use Gemini\Responses\GenerativeModel\GenerateContentResponse;
 use Gemini\Testing\Resources\Concerns\Testable;
+use Generator;
 
 final class ChatSessionTestResource implements ChatSessionContract
 {
@@ -20,9 +22,14 @@ final class ChatSessionTestResource implements ChatSessionContract
         return ChatSession::class;
     }
 
-    public function sendMessage(string|array|Blob|Content ...$parts): GenerateContentResponse
+    public function sendMessage(string|Blob|array|Content|UploadedFile ...$parts): GenerateContentResponse
     {
         return $this->record(method: __FUNCTION__, args: func_get_args(), model: $this->model);
+    }
+
+    public function streamSendMessage(string|Blob|array|Content|UploadedFile ...$parts): Generator
+    {
+        yield $this->record(method: __FUNCTION__, args: func_get_args(), model: $this->model);
     }
 
     /**
